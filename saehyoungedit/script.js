e67980d36facb64e71adaecc08fb6202d1d5444c
litarrow <litarrow@gmail.com>
2019-11-02 19:19:44 -0700

    Revert "Update script.js"
    This reverts commit 5b71303d52b920544d63f686ca7a6b0b2555b0be.

--- saehyoungedit/script.js
+++ saehyoungedit/script.js
@@ -41,10 +41,10 @@ app.get('/account', function (req, res) {
 
 });
 
-app.get('/product',function(req,res){
+app.get('/products',function(req,res){
 
   var context = {};
-  res.render('product', context);
+  res.render('products', context);
 
 });
 
@@ -52,6 +52,13 @@ app.get('/cart', function (req, res) {
 
 	var context = {};
 	res.render('cart', context);
+
+});
+
+app.get('/category', function (req, res) {
+
+	var context = {};
+	res.render('category', context);
 
 });
 
@@ -67,109 +74,6 @@ app.get('/payment', function (req, res) {
 	var context = {};
 	res.render('payment', context);
 
-});
-
-app.get('/seller', function (req, res) {
-
-	var context = {};
-	res.render('seller', context);
-
-});
-
-app.get('/create',function(req,res){
-
-  var context = {};
-  res.render('create', context);
-
-});
-
-app.get('/login',function(req,res){
-
-  var context = {};
-  res.render('login', context);
-
-});
-
-app.get('/settings',function(req,res){
-
-  var context = {};
-  res.render('settings', context);
-
-});
-
-app.get('/ordersdb',function(req,res){
-  var context = {};
-  var params = [];
-
-  pool.query('select * from actor', (err, rows) => {
-    for (var p in req.body)
-    {
-      params.push({'orderNumber': p});
-    }
-    console.log(rows);
-    context.data = rows;
-  });
-
-  req.addEventListener('load', function() {
-    if (req.status >= 200 && req.status < 400) {
-      console.log(JSON.parse(req.responseText));
-      document.getElementById("data").textContent = req.responseText;
-    } else {
-      console.log("Error in network request: " + req.statusText);
-      document.getElementById("data").textContent = "Error in network request: " + req.statusText;
-    }
-  });
-
-  context.list = params;
-  // console.log(params);
-
-  pool.end((err) => {
-    // The connection is terminated gracefully
-    // Ensures all previously enqueued queries are still
-    // before sending a COM_QUIT packet to the MySQL server.
-  });
-
-  res.render('ordersdb', context);
-
-});
-
-app.get('/cartdb',function(req,res){
-
-  var context = {};
-  res.render('cartdb', context);
-
-});
-
-app.get('/productsdb',function(req,res){
-
-  var context = {};
-  res.render('productsdb', context);
-
-});
-
-app.get('/addproducts',function(req,res){
-
-  var context = {};
-  res.render('addproducts', context);
-
-});
-
-app.post('/confirmed',function(req, res, next){
-  var context = {};
-  var params = [];
-
-  if(req.body['submitAddCard']) {
-    var card_num = req.body.card_num;
-
-    context.card_num = card_num;
-    req.session.card_num = card_num;
-
-    res.render('confirmed', context);
-  }
-  else {
-    console.log('req.body["submitAddCard"] not detected');
-    console.log(req.body);
-  }
 });
 
 //exceptions handling
