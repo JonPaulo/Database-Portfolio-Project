f13d460512ba9eb7ebf033aaf6316d834a7bcf19
JonPaulo <bautijon@oregonstate.edu>
2019-11-16 21:56:28 -0800

    add functional account page

--- dumbAmazonCode/account.js
+++ dumbAmazonCode/account.js
@@ -1,9 +1,24 @@
-﻿module.exports = function () {
+module.exports = function () {
 	var express = require('express');
 	var router = express.Router();
 
-	function getAccount(res, mysql, context, complete) {
-		mysql.pool.query("SELECT id, username, password, email, fname, lname, street, city, zip FROM account", function (error, results, fields) {
+		function getAccount(res, mysql, context, complete) {
+			mysql.pool.query("SELECT id, username, password, email, fname, lname, street, city, zip FROM account", function (error, results, fields) {
+				if (error) {
+					res.write(JSON.stringify(error));
+					res.end();
+				}
+				context.account = results;
+				complete();
+			});
+		}
+
+	/* Find categories whose name includes the given string in the req */
+	function searchFunction(req, res, mysql, context, complete) {
+		//sanitize the input as well as include the % character
+		var query = "SELECT id, username, password, email, fname, lname, street, city, zip FROM account WHERE " + req.query.filter + " LIKE " + mysql.pool.escape('%' + req.query.search + '%');
+		console.log(query)
+		mysql.pool.query(query, function (error, results, fields) {
 			if (error) {
 				res.write(JSON.stringify(error));
 				res.end();
@@ -17,7 +32,6 @@
 	router.get('/', function (req, res) {
 		var callbackCount = 0;
 		var context = {};
-		context.jsscripts = ["deleteAccount.js", "searchCategories.js"];
 		var mysql = req.app.get('mysql');
 		getAccount(res, mysql, context, complete);
 		function complete() {
@@ -29,41 +43,64 @@
 		}
 	});
 
-	// function getAccountAfterUpdate(res, mysql, context, id, complete) {
-	// 	var sql = "SELECT id, username, password, email, fname, lname, street, city, zip FROM account";
-	// 	var inserts = [id];
-	// 	mysql.pool.query(sql, inserts, function (error, results, fields) {
-	// 		if (error) {
-	// 			res.write(JSON.stringify(error));
-	// 			res.end();
-	// 		}
-	// 		context.account = results[0];
-	// 		complete();
-	// 	});
-	// }
+	/*Display all categories whose name starts with a given string. */
+	router.get('/search', function (req, res) {
+		var callbackCount = 0;
+		var context = {};
+		var mysql = req.app.get('mysql');
 
-	/* Display one categories for the specific purpose of updating categories */
-	// router.get('/:id', function (req, res) {
-	// 	callbackCount = 0;
-	// 	var context = {};
-	// 	context.jsscripts = ["updateAccount.js"];
-	// 	var mysql = req.app.get('mysql');
-	// 	getAccountAfterUpdate(res, mysql, context, req.params.id, complete);
-	// 	function complete() {
-	// 		callbackCount++;
-	// 		if (callbackCount >= 1) {
-	// 			res.render('account', context);
-	// 		}
-	//
-	// 	}
-	// });
+		searchFunction(req, res, mysql, context, complete);
+		function complete() {
+			callbackCount++;
+			if (callbackCount >= 1) {
+				res.render('account', context);
+			}
+		}
+	});
 
+	/* Adds a categories, redirects to the categories page after adding */
+	router.post('/add', function (req, res) {
+		console.log(req.body)
+		var mysql = req.app.get('mysql');
+		var sql = "INSERT INTO account (username, password, email, fname, lname, street, city, zip) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+		var inserts = [req.body.newUsername, req.body.newPassword, req.body.newEmail,
+			req.body.newFname, req.body.newLname, req.body.newStreet, req.body.newCity,
+			req.body.newZip];
+		sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
+			if (error) {
+				console.log(JSON.stringify(error))
+				res.write(JSON.stringify(error));
+				res.end();
+			} else {
+				res.redirect('/account');
+			}
+		});
+	});
 
-	/* Route to delete a categories, simply returns a 202 upon success. Ajax will handle this. */
-	router.delete('/:id', function (req, res) {
+	/* updates a categories, redirects to the categories page after adding */
+	router.post('/update', function (req, res) {
+		console.log(req.body)
+		var mysql = req.app.get('mysql');
+		var sql = "UPDATE account SET username = ?, password=?, email=?, fname=?, lname=?, street=?, city=?, zip=? WHERE id = ?";
+		var inserts = [req.body.editUsername, req.body.editPassword, req.body.editEmail,
+			req.body.editFname, req.body.editLname, req.body.editStreet, req.body.editCity,
+			req.body.editZip, req.body.updateID];
+		sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
+			if (error) {
+				console.log(JSON.stringify(error))
+				res.write(JSON.stringify(error));
+				res.end();
+			} else {
+				res.redirect('/account');
+			}
+		});
+	});
+
+	/* delete a categories, redirects to the categories page after deleting */
+	router.post('/delete', function (req, res) {
 		var mysql = req.app.get('mysql');
 		var sql = "DELETE FROM account WHERE id = ?";
-		var inserts = [req.params.id];
+		var inserts = [req.body.deleteID];
 		sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
 			if (error) {
 				console.log(error)
@@ -71,7 +108,7 @@
 				res.status(400);
 				res.end();
 			} else {
-				res.status(202).end();
+				res.redirect('/account');
 			}
 		})
 	})
