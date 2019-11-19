5e3792ea71cfcce94ad8ad03120874e9e111602f
litarrow <litarrow@gmail.com>
2019-11-18 23:52:11 -0800

    orders_product CRUD works, minor updates
    orders_product to orders.order_total calculation still required

--- dumbAmazonCode/public/js/orders_product.js
+++ dumbAmazonCode/public/js/orders_product.js
@@ -20,7 +20,7 @@
      * fname+lname or id column
      */
 	function getOrders_product(req, res, mysql, context, complete) {
-		sql = "SELECT product.id, product.name, product.price, quantity, subtotal FROM orders_product INNER JOIN product on orders_product.product_id = product.id WHERE orders_id = " + mysql.pool.escape(req.query.id);
+		sql = "SELECT orders_id, product_id, product.name, product.price, quantity, subtotal FROM orders_product INNER JOIN product on orders_product.product_id = product.id WHERE orders_id = " + mysql.pool.escape(req.query.id);
 		//sql = "SELECT pid, cid, CONCAT(fname,' ',lname) AS name, title AS certificate FROM bsg_people INNER JOIN bsg_cert_people on bsg_people.character_id = bsg_cert_people.pid INNER JOIN bsg_cert on bsg_cert.certification_id = bsg_cert_people.cid ORDER BY name, certificate"
 		mysql.pool.query(sql, function (error, results, fields) {
 			if (error) {
@@ -32,8 +32,20 @@
 		});
 	}
 
-	function getPrice(req, res, mysql, context, complete) {
+	function getNewPrice(req, res, mysql, context, complete) {
 		sql = "SELECT price, inventory FROM product WHERE id = " + mysql.pool.escape(req.body.newProductID);
+		mysql.pool.query(sql, function (error, results, fields) {
+			if (error) {
+				res.write(JSON.stringify(error));
+				res.end()
+			}
+			context.product = results
+			complete();
+		});
+	}
+
+	function getUpdatePrice(req, res, mysql, context, complete) {
+		sql = "SELECT price, inventory FROM product WHERE id = " + mysql.pool.escape(req.body.updateProductID);
 		mysql.pool.query(sql, function (error, results, fields) {
 			if (error) {
 				res.write(JSON.stringify(error));
@@ -73,7 +85,7 @@
 		var mysql = req.app.get('mysql');
 
 		// get the price and inventory of the item first
-		getPrice(req, res, mysql, context1, complete);
+		getNewPrice(req, res, mysql, context1, complete);
 		function complete() {
 			callbackCount++;
 			if (callbackCount >= 1) {
@@ -94,27 +106,53 @@
 		}
 	});
 
-	///* Delete a person's certification record */
- //   /* This route will accept a HTTP DELETE request in the form
- //    * /pid/{{pid}}/cert/{{cid}} -- which is sent by the AJAX form 
- //    */
-	//router.delete('/pid/:pid/cert/:cid', function (req, res) {
-	//	//console.log(req) //I used this to figure out where did pid and cid go in the request
-	//	console.log(req.params.pid)
-	//	console.log(req.params.cid)
-	//	var mysql = req.app.get('mysql');
-	//	var sql = "DELETE FROM bsg_cert_people WHERE pid = ? AND cid = ?";
-	//	var inserts = [req.params.pid, req.params.cid];
-	//	sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
-	//		if (error) {
-	//			res.write(JSON.stringify(error));
-	//			res.status(400);
-	//			res.end();
-	//		} else {
-	//			res.status(202).end();
-	//		}
-	//	})
-	//})
+	/* updates a orders_product, redirects to the orders_product page after adding */
+	router.post('/update', function (req, res) {
+		console.log(req.body);
+		var callbackCount = 0;
+		var context1 = {};
+		var mysql = req.app.get('mysql');
+
+		// get the price and inventory of the item first
+		getUpdatePrice(req, res, mysql, context1, complete);
+		function complete() {
+			callbackCount++;
+			if (callbackCount >= 1) {
+				// update the item
+				console.log(context1.product[0].price);
+				console.log(req.body.updateQuantity);
+				var total = context1.product[0].price * req.body.updateQuantity;
+				var sql = "UPDATE orders_product SET quantity = ?, subtotal = ? WHERE orders_id = ? AND product_id = ?";
+				var inserts = [req.body.updateQuantity, total, req.body.updateOrdersID, req.body.updateProductID];
+				sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
+					if (error) {
+						console.log(JSON.stringify(error))
+						res.write(JSON.stringify(error));
+						res.end();
+					} else {
+						res.redirect('/orders_product?id=' + req.body.updateOrdersID);
+					}
+				});
+			}
+		}
+	});
+
+	/* delete a orders_product, redirects to the orders_product page after deleting */
+	router.post('/delete', function (req, res) {
+		var mysql = req.app.get('mysql');
+		var sql = "DELETE FROM orders_product WHERE orders_id = ? AND product_id = ?";
+		var inserts = [req.body.ordersID, req.body.productID];
+		sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
+			if (error) {
+				console.log(error)
+				res.write(JSON.stringify(error));
+				res.status(400);
+				res.end();
+			} else {
+				res.redirect('/orders_product?id=' + req.body.ordersID);
+			}
+		})
+	})
 
 	return router;
 }();
