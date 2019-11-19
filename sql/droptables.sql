c4e5fa09f473b019142c346e682a22c6e8e3dbce
JonPaulo <bautijon@oregonstate.edu>
2019-11-18 20:54:44 -0800

    Revert "Merge branch 'master' of https://github.com/JonPaulo/CS340-Project"
    This reverts commit c3ffb545f1307d5f4f7760b07198a6c7540c5feb, reversing
    changes made to 04e8508f7c074873c3d5198c460198cbde253112.

--- sql/droptables.sql
+++ sql/droptables.sql
@@ -2,12 +2,9 @@
 -- The order is specific. Tables can only be dropped if there is no FOREIGN KEY referencing it.
 -- Therefore, orders_product must be dropped first.
 
-SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `orders_product`;
 DROP TABLE IF EXISTS `orders`;
 DROP TABLE IF EXISTS `payment`;
 DROP TABLE IF EXISTS `account`;
 DROP TABLE IF EXISTS `product`;
 DROP TABLE IF EXISTS `categories`;
-DROP TABLE IF EXISTS `orders_product`;
-SET FOREIGN_KEY_CHECKS = 1;
