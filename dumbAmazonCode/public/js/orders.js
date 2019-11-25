12489d9a93759eaadd4a4da5899028ad16191ef8
litarrow <litarrow@gmail.com>
2019-11-24 18:47:41 -0800

    delete for orders page

--- dumbAmazonCode/public/js/orders.js
+++ dumbAmazonCode/public/js/orders.js
@@ -5,24 +5,14 @@ module.exports = function () {
 	var router = express.Router();
 	var moment = require('moment');
 
-	function getOrders(res, mysql, context, complete) {
-		mysql.pool.query("SELECT id as oid, user_id, payment_id, order_date FROM orders", function (error, results, fields) {
-			if (error) {
-				res.write(JSON.stringify(error));
-				res.end();
-			}
-			context.orders = results;
-			complete();
-		});
-	}
-
 	function getOrders_account(res, mysql, context, complete) {
-		mysql.pool.query("SELECT orders.id, username, payment_id, order_date FROM account INNER JOIN orders on orders.user_id = account.id", function (error, results, fields) {
+		mysql.pool.query("SELECT orders.id as id, username, payment_id, order_date FROM account INNER JOIN orders on orders.user_id = account.id", function (error, results, fields) {
 			if (error) {
 				res.write(JSON.stringify(error));
 				res.end();
 			}
 			context.orders_account = results;
+			orderSubtotals = new Object();
 			var i;
 			for (i = 0; i < results.length; i++) {
 				if ((context.orders_account)[i].id in orderSubtotals) {
@@ -33,31 +23,6 @@ module.exports = function () {
 				}
 				(context.orders_account)[i].order_date = moment((context.orders_account)[i].order_date).format('MM/DD/YYYY');
 			}
-			complete();
-		});
-	}
-
-	function getOrders_product(res, mysql, context, complete) {
-		mysql.pool.query("SELECT orders_id, product_id, quantity, subtotal FROM orders_product", function (error, results, fields) {
-			if (error) {
-				res.write(JSON.stringify(error));
-				res.end();
-			}
-			context.orders_product = results;
-			orderSubtotals = new Object();
-			var i;
-			for (i = 0; i < results.length; i++) {
-				if (!(results[i].orders_id in orderSubtotals)) {
-					orderSubtotals[results[i].orders_id] = results[i].subtotal;
-				}
-				else {
-					orderSubtotals[results[i].orders_id] += results[i].subtotal;
-				}
-			}
-			// console.log(context.orders_product);
-			// console.log(results[0].subtotal);
-			context.order_subtotal = orderSubtotals;
-			console.log(orderSubtotals);
 			complete();
 		});
 	}
@@ -88,7 +53,6 @@ module.exports = function () {
 	function searchFunction(req, res, mysql, context, complete) {
 		//sanitize the input as well as include the % character
 		var query = "SELECT id, user_id, payment_id, order_date FROM orders WHERE " + req.query.filter + " LIKE " + mysql.pool.escape('%' + req.query.search + '%');
-		console.log(query)
 		mysql.pool.query(query, function (error, results, fields) {
 			if (error) {
 				res.write(JSON.stringify(error));
@@ -104,14 +68,12 @@ module.exports = function () {
 		var callbackCount = 0;
 		var context = {};
 		var mysql = req.app.get('mysql');
-		//getOrders(res, mysql, context, complete);
-		getOrders_product(res, mysql, context, complete);
 		getOrders_account(res, mysql, context, complete);
 		getPayment_account(res, mysql, context, complete);
 		getAccount(res, mysql, context, complete);
 		function complete() {
 			callbackCount++;
-			if (callbackCount >= 4) {
+			if (callbackCount >= 3) {
 				res.render('orders', context);
 			}
 
@@ -134,9 +96,8 @@ module.exports = function () {
 
 	/* Adds an order, redirects to the order page after adding */
 	router.post('/add', function (req, res) {
-		console.log(req.body)
 		var mysql = req.app.get('mysql');
-		var sql = "INSERT INTO orders (user_id, payment_id, order_date) VALUES (?, ?, 0, ?)";
+		var sql = "INSERT INTO orders (user_id, payment_id, order_date) VALUES (?, ?, ?)";
 		var inserts = [req.body.newUserID, req.body.newPaymentID, req.body.newDate];
 		sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
 			if (error) {
@@ -151,7 +112,6 @@ module.exports = function () {
 
 	/* updates an order, redirects to the order page after adding */
 	router.post('/update', function (req, res) {
-		console.log(req.body)
 		var mysql = req.app.get('mysql');
 		var sql = "UPDATE orders SET user_id=?, payment_id=?, order_date=? WHERE id = ?";
 		var inserts = [req.body.updateUserID, req.body.updatePaymentID, req.body.updateOrderDate, req.body.updateID];
@@ -170,7 +130,7 @@ module.exports = function () {
 	router.post('/delete', function (req, res) {
 		var mysql = req.app.get('mysql');
 		var sql = "DELETE FROM orders WHERE id = ?";
-		var inserts = [req.body.deleteID];
+		var inserts = [req.body.id];
 		sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
 			if (error) {
 				console.log(error)
